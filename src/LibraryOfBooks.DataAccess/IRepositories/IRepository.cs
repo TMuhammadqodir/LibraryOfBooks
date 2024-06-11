d787887c0002cc53ce35b7e903e2e42ab653e15b
Muhammadqodir Turdiyev <118986233+TMuhammadqodir@users.noreply.github.com>
2024-06-11 22:48:24 +0500

    Add Repository

---
+++ src/LibraryOfBooks.DataAccess/IRepositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using LibraryOfBooks.Domain.Commons;
+using System.Linq.Expressions;
+
+namespace LibraryOfBooks.Dataccess.IRepositories;
+
+public interface IRepository <TEntity> where TEntity : Auditable
+{
+    ValueTask<TEntity> InsertAsync(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+    void Destroy(TEntity entity);
+    ValueTask<TEntity> SelectAsync(Expression<Func<TEntity, bool>> expression, string[] includes = null);
+    IQueryable<TEntity> SelectAll(
+        Expression<Func<TEntity, bool>> expression = null, string[] includes = null, bool isTracking = true);
+    ValueTask<bool> SaveAsync();
+}
