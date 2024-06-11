b0bdd38e0e1c8db85b38c4ae620e6e9f8e2fa482
Muhammadqodir Turdiyev <118986233+TMuhammadqodir@users.noreply.github.com>
2024-06-11 22:34:39 +0500

    AddMigration

--- src/LibraryOfBooks.DataAccess/Contexts/AppDbContext.cs
+++ src/LibraryOfBooks.DataAccess/Contexts/AppDbContext.cs
@@ -12,4 +12,13 @@ public class AppDbContext : DbContext
     public DbSet<Asset> Assets { get; set; }
     public DbSet<Favorite> Favorites { get; set; }
     public DbSet<BookCategory> BookCategories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>().HasQueryFilter(u => !u.IsDeleted);
+        modelBuilder.Entity<User>().HasQueryFilter(u => !u.IsDeleted);
+        modelBuilder.Entity<Asset>().HasQueryFilter(u => !u.IsDeleted);
+        modelBuilder.Entity<Favorite>().HasQueryFilter(u => !u.IsDeleted);
+        modelBuilder.Entity<BookCategory>().HasQueryFilter(u => !u.IsDeleted);
+    }
 }
