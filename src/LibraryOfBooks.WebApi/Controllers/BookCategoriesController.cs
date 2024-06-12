1a7141d450fce9b3e1d4554737c442415e484c94
Muhammadqodir Turdiyev <118986233+TMuhammadqodir@users.noreply.github.com>
2024-06-12 14:04:52 +0500

    Add controllers

---
+++ src/LibraryOfBooks.WebApi/Controllers/BookCategoriesController.cs
@@ -0,0 +1,64 @@
+﻿using LibraryOfBooks.Service.DTOs.BookCategories;
+using LibraryOfBooks.Service.Interfaces;
+using LibraryOfBooks.Service.Services;
+using LibraryOfBooks.WebApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryOfBooks.WebApi.Controllers;
+
+public class BookCategoriesController : BaseController
+{
+    private readonly IBookCategoryService bookCategoryService;
+    public BookCategoriesController(IBookCategoryService bookCategoryService)
+    {
+        this.bookCategoryService = bookCategoryService;
+    }
+
+    [HttpPost("create")]
+    public async ValueTask<IActionResult> PostAsync(BookCategoryCreationDto dto)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Succes",
+            Data = await bookCategoryService.AddAsync(dto)
+        });
+
+    [HttpPost("update")]
+    public async ValueTask<IActionResult> UpdateAsync(BookCategoryUpdateDto dto)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await bookCategoryService.ModifyAsync(dto)
+        });
+
+    [HttpDelete("delete/{id:long}")]
+    public async ValueTask<IActionResult> DeleteAsync(long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await bookCategoryService.DeleteAsync(id)
+        });
+
+    [AllowAnonymous]
+    [HttpGet("get/{id:long}")]
+    public async ValueTask<IActionResult> GetAsync(long id)
+        => Ok(new Response
+        {
+            StatusCode=200,
+            Message = "Success",
+            Data = await bookCategoryService.RetrieveByIdAsync(id)
+        });
+
+    [AllowAnonymous]
+    [HttpGet("get-all")]
+    public async ValueTask<IActionResult> GetAllAsync()
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await bookCategoryService.RetrieveAllAsync()
+        });
+}
