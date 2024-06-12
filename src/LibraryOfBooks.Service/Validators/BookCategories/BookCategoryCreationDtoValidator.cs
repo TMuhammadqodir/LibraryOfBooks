41595cc744fa64f0f569f8457b9f874231003aad
Muhammadqodir Turdiyev <118986233+TMuhammadqodir@users.noreply.github.com>
2024-06-13 03:02:14 +0500

    Add Validators

---
+++ src/LibraryOfBooks.Service/Validators/BookCategories/BookCategoryCreationDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using LibraryOfBooks.Service.DTOs.BookCategories;
+
+namespace LibraryOfBooks.Service.Validators.BookCategories;
+
+public class BookCategoryCreationDtoValidator : AbstractValidator<BookCategoryCreationDto>
+{
+    public BookCategoryCreationDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+                .WithMessage("Name is required.")
+            .Length(1, 64)
+                .WithMessage("Name must be between 1 and 64 characters.");
+    }
+}
