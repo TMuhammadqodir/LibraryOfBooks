5c32f034d73e4a60bbcb739a1a454ed7f0f666af
Muhammadqodir Turdiyev <118986233+TMuhammadqodir@users.noreply.github.com>
2024-06-15 15:44:42 +0500

    Fixed complated

--- src/LibraryOfBooks.WebApi/Program.cs
+++ src/LibraryOfBooks.WebApi/Program.cs
@@ -5,6 +5,7 @@ using LibraryOfBooks.WebApi.Middlewares;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 using System;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//builder.Services.ConfigureSwagger();
 
 //Add DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
